e68e1df40814d1092cc45219259ed85149d78621
David Corbitt <david@realityplatforms.org>
2022-08-24 19:38:48 -0600

    Only request camera permission

--- src/Permissions.ts
+++ src/Permissions.ts
@@ -1,21 +1,13 @@
 import {Platform} from 'react-native';
 import {request, Permission, PERMISSIONS} from 'react-native-permissions';
 
-type PermsType = 'location' | 'microphone' | 'activity' | 'camera';
+type PermsType = 'microphone' | 'camera';
 
 const permissions = {
-  location: Platform.select({
-    ios: PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-    android: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-  }),
-  microphone: Platform.select({
-    ios: PERMISSIONS.IOS.MICROPHONE,
-    android: PERMISSIONS.ANDROID.RECORD_AUDIO,
-  }),
-  activity: Platform.select({
-    ios: PERMISSIONS.IOS.MOTION,
-    android: PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION,
-  }),
+  //   microphone: Platform.select({
+  //     ios: PERMISSIONS.IOS.MICROPHONE,
+  //     android: PERMISSIONS.ANDROID.RECORD_AUDIO,
+  //   }),
   camera: Platform.select({
     ios: PERMISSIONS.IOS.CAMERA,
     android: PERMISSIONS.ANDROID.CAMERA,
